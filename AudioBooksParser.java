f486efc7fed5e46eacd2d8e7758d79ab3dedef68
unknown <samantha1999@live.com.sg>
2020-02-13 19:34:53 +0800

    convert duration

--- AudioBooksParser.java
+++ AudioBooksParser.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -8,20 +9,11 @@ public class AudioBooksParser{
 
     private AudioBooksParser(){}
 
-    public static List<AudioBook> parseAudioBookFile(){
-        String filename = "AudioBooks.txt";
-        Scanner in = null;
-        try{
-            in = new Scanner(new File(filename));
-        } catch(IOException e){
-            System.err.println("File " + filename + " does not exist.");
-            return null;
-        }
-
-        //skip first 2 lines
-        in.nextLine();
-        in.nextLine();
-
+    public static List<AudioBook> parseAudioBookFile(String filename){
+        Scanner in = getScanner(filename);
+		if(in == null) {
+			return null;
+		}
         List<AudioBook> audioBooks = new ArrayList<AudioBook>();
         while(in.hasNextLine()){
             String line = in.nextLine();
@@ -32,9 +24,25 @@ public class AudioBooksParser{
         return audioBooks;
     }
 
+    private static Scanner getScanner(String filename) {
+		Scanner in = null;
+		
+		try {
+			in = new Scanner(new FileInputStream(filename), "UTF-8");
+		} catch (IOException e) {
+			System.err.println("File "+filename+" does not exist.");
+			return null;
+		} 
+		//two non-book lines to skip:
+		in.nextLine();
+		in.nextLine();
+		
+		return in;
+	}
+
     private static AudioBook parseAudioBook(String line){
         String[] data = line.split(";");
-        for(int i=9;i<data.length;i++){
+        for(int i=0;i<data.length;i++){
             data[i] = data[i].trim();
         }
         String author = data[0];
@@ -44,7 +52,11 @@ public class AudioBooksParser{
 		int yearOfPub = Integer.parseInt(data[4]);
 		String asin = data[5];
         String narrator = data[6];
-        Duration listenTime = data[7];
+
+        String[] timeData = data[7].split(" ");
+        int hours = Integer.parseInt(timeData[0]);
+        int minutes = Integer.parseInt(timeData[3]);
+        Duration listenTime = new Duration(hours, minutes);
         AudioBook ab = new AudioBook(author, title, genre, publisher, yearOfPub, asin, narrator, listenTime);
     
         return ab;
@@ -58,8 +70,8 @@ public class AudioBooksParser{
 
     public static void main(String[] args) {
 		//main for testing this class
-		List<AudioBook> aBooks = AudioBooksParser.parseAudioBookFile();
-		display(aBooks);
+		List<AudioBook> aBooks = AudioBooksParser.parseAudioBookFile("AudioBooks.txt");
+		System.out.println(aBooks);
 	}
 
 }
