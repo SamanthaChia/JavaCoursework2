bd8591c2856caaf1f636777b0e33c0363c95710a
unknown <samantha1999@live.com.sg>
2020-02-13 21:57:52 +0800

    added name, sn cn

--- AudioBooksParser.java
+++ AudioBooksParser.java
@@ -1,3 +1,8 @@
+// Name: Samantha Chia Eileen 
+// Student Number: 190428594
+// Course Number : CO1109
+// Coursework 2
+
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
